394ec94c240960245ef0eb13a71fbd73e99be46b
Inna Znaida <IZN@simcorp.com>
2018-06-08 16:35:22 +0300

    viewmodel added

---
+++ FriendOrganizer.UI/ViewModel/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using FriendOrganizer.Model;
+using FriendOrganizer.UI.Data;
+
+namespace FriendOrganizer.UI.ViewModel {
+    public class MainViewModel {
+        private Friend _selectedFriend;
+        private IFriendDataService _friendDataService;
+
+        public Friend SelectedFriend
+        {
+            get { return _selectedFriend; }
+            set { _selectedFriend = value; }
+        }
+
+        public ObservableCollection<Friend> Friends { get; set; }
+        
+        public MainViewModel(IFriendDataService frienddataService) {
+            Friends = new ObservableCollection<Friend>();
+            _friendDataService = frienddataService;
+        }
+
+        public void Load() {
+            var friends = _friendDataService.GetAll();
+            Friends.Clear();
+            foreach (var fr in friends) {
+                Friends.Add(fr);
+            }
+
+        }
+    }
+}
