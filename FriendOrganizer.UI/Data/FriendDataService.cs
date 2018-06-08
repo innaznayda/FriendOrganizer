1d201971f9f3b01688c1ba9453659bec33bdea5d
Inna Znaida <IZN@simcorp.com>
2018-06-08 15:30:38 +0300

    add dummy dataservice

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using FriendOrganizer.Model;
+
+namespace FriendOrganizer.UI.Data {
+    public class FriendDataService : IFriendDataService {
+        public IEnumerable<Friend> GetAll() {
+
+            //TODO: load real data from DB
+            yield return new Friend {FirstName = "John", LastName = "Johnson"};
+            yield return new Friend { FirstName = "Jack", LastName = "Jackson" };
+            yield return new Friend { FirstName = "Dick", LastName = "Dickson" };
+            yield return new Friend { FirstName = "Rick", LastName = "Rickson" };
+        }
+    }
+}
