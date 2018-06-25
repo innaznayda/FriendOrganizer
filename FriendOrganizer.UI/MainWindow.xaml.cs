c3b181ea7c3130c131b538784399ee3ddffdc756
Inna Znaida <IZN@simcorp.com>
2018-06-25 09:49:48 +0300

    add bootstrapper and dbcontext

--- FriendOrganizer.UI/MainWindow.xaml.cs
+++ FriendOrganizer.UI/MainWindow.xaml.cs
@@ -6,8 +6,6 @@ namespace FriendOrganizer.UI {
    public partial class MainWindow : Window {
        private MainViewModel _viewModel;
         public MainWindow(MainViewModel viewModel) {
-            
-             
             InitializeComponent();
             _viewModel = viewModel;
             DataContext = _viewModel;
